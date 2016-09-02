6399381e6b53a25b05519ac23936a97145eea605
Hung Nguyen <hnguyenworkstation@gmail.com>
2016-09-01 21:02:46 -0400

    Map works

--- fmdeals/app/src/main/java/com/greenfam/fmdeals/Fragments/CommunicationFragment.java
+++ fmdeals/app/src/main/java/com/greenfam/fmdeals/Fragments/CommunicationFragment.java
@@ -1,11 +1,22 @@
 package com.greenfam.fmdeals.Fragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.greenfam.fmdeals.Helpers.PermissionUtils;
 import com.greenfam.fmdeals.R;
 
 /**
--- fmdeals/app/src/main/java/com/greenfam/fmdeals/Fragments/LocalBusinessFragment.java
+++ fmdeals/app/src/main/java/com/greenfam/fmdeals/Fragments/LocalBusinessFragment.java
@@ -1,40 +1,247 @@
 package com.greenfam.fmdeals.Fragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.greenfam.fmdeals.Helpers.PermissionUtils;
 import com.greenfam.fmdeals.R;
 
 /**
  * Created by quang on 8/31/2016.
  */
-public class LocalBusinessFragment extends Fragment {
-    private static View view;
+public class LocalBusinessFragment extends Fragment
+        implements OnMapReadyCallback,
+        GoogleMap.OnMyLocationButtonClickListener,
+        ActivityCompat.OnRequestPermissionsResultCallback,
+        GoogleMap.OnCameraMoveStartedListener,
+        GoogleMap.OnCameraMoveListener,
+        GoogleMap.OnCameraMoveCanceledListener,
+        GoogleMap.OnCameraIdleListener {
+
+    private MapView mMapView;
+
+    /**
+     * Request code for location permission request.
+     *
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    /**
+     * Flag indicating whether a requested permission has been denied after returning in
+     * {@link #onRequestPermissionsResult(int, String[], int[])}.
+     */
+    private boolean mPermissionDenied = false;
+
+    private GoogleMap googleMap;
+
+    private View rootView;
 
     public LocalBusinessFragment() {
         // Required empty public constructor
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        if (view != null) {
-            ViewGroup parent = (ViewGroup) view.getParent();
+
+        if (rootView != null) {
+            ViewGroup parent = (ViewGroup) rootView.getParent();
+
             if (parent != null)
-                parent.removeView(view);
+                parent.removeView(rootView);
         }
         try {
-            view = inflater.inflate(R.layout.map_fragment_activity, container, false);
+            rootView = inflater.inflate(R.layout.local_business_fragment, container, false);
+            enableMyLocation();
+            switch (GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity()) )
+            {
+                case ConnectionResult.SUCCESS:
+                    Toast.makeText(getActivity(), "SUCCESS", Toast.LENGTH_SHORT).show();
+                    mMapView = (MapView) rootView.findViewById(R.id.map);
+                    mMapView.onCreate(savedInstanceState);
+
+                    mMapView.onResume(); // needed to get the map to display immediately
+                    mMapView.getMapAsync(this);
+                    break;
+                case ConnectionResult.SERVICE_MISSING:
+                    Toast.makeText(getActivity(), "SERVICE MISSING", Toast.LENGTH_SHORT).show();
+                    break;
+                case ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED:
+                    Toast.makeText(getActivity(), "UPDATE REQUIRED", Toast.LENGTH_SHORT).show();
+                    break;
+                default: Toast.makeText(getActivity(), GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity()), Toast.LENGTH_SHORT).show();
+            }
+
         } catch (InflateException e) {
             /* map is already there, just return view as it is */
         }
-        return view;
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return rootView;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map){
+        googleMap = map;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        googleMap.setOnMyLocationButtonClickListener(this);
+        googleMap.setOnCameraIdleListener(this);
+        googleMap.setOnCameraMoveStartedListener(this);
+        googleMap.setOnCameraMoveListener(this);
+        googleMap.setOnCameraMoveCanceledListener(this);
+
+        // For showing a move to my location button
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            enableMyLocation();
+            return;
+        }
+
+        googleMap.setMyLocationEnabled(true);
+
+        // For dropping a marker at a point on the Map
+        LatLng sydney = new LatLng(-34, 151);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+        // For zooming automatically to the location of the marker
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(15).build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private void enableMyLocation(){
+            System.out.print("get here");
+            if(ContextCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED){
+                // Permission to access the location is missing.
+                PermissionUtils.requestPermission(this.getActivity(),LOCATION_PERMISSION_REQUEST_CODE,
+                Manifest.permission.ACCESS_FINE_LOCATION,true);
+            }else if(googleMap!=null){
+                // Access to the location has been granted to the app.
+                googleMap.setMyLocationEnabled(true);
+            }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick(){
+            Toast.makeText(this.getContext(),"MyLocation button clicked",Toast.LENGTH_SHORT).show();
+            // Return false so that we don't consume the event and the default behavior still occurs
+            // (the camera animates to the user's current position).
+            return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getFragmentManager(), "dialog");
+    }
+
+
+    @Override
+    public void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+
+
+    @Override
+    public void onCameraIdle(){
+
+    }
+
+    @Override
+    public void onCameraMoveCanceled(){
+
+    }
+
+    @Override
+    public void onCameraMove(){
+
+    }
+
+    @Override
+    public void onCameraMoveStarted(int i){
+
     }
 }
